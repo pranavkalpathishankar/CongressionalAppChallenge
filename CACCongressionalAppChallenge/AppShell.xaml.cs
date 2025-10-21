8273ee18d6367c713ebd8a5e728d944c67cc63cb
pranavkalpathishankar <pranavkalpathishankar@outlook.com>
2025-10-20 22:11:08 -0500

    Add project files.

---
+++ CACCongressionalAppChallenge/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CACCongressionalAppChallenge;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ CACCongressionalAppChallenge/MainPage.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace CACCongressionalAppChallenge;
+
+public partial class MainPage : ContentPage
+{
+    public MainPage()
+    {
+        InitializeComponent();
+    }
+}
